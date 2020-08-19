c5f4526d820011aef87602160cffe7b04e85c68a
JC Marin <jc@b1it.com>
2020-08-19 12:17:26 -0400

    added remote

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import "./Login.css";
+import { Button } from "@material-ui/core";
 
 function Login() {
+    const signIn = (e) => { 
+        
+        
+
+    }
     return (
         <div className="login">
             <div className="login__container">
-                <img src="https://cdn.brandfolder.io/5H442O3W/as/pnlpsg-beh8pk-2mgxnk/appIcon_desktop.png" alt="" />
+                 <img src="https://cdn.brandfolder.io/5H442O3W/as/pnlpsg-beh8pk-2mgxnk/appIcon_desktop.png" alt="" /> 
                 <h1>Sign in to HQ</h1>
                 <p>whatever.slack.com</p>
+                <Button onClick={signIn}>Sign In with Google</Button>
             </div>
+            
         </div>
     )
 }
